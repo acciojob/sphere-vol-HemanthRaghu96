b0e83ed934ac11b5dce2c92b112c01b9bde85421
Hemanth R <91587889+HemanthRaghu96@users.noreply.github.com>
2025-12-04 19:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function volume_sphere() {
     //Write your code here
+	let radius=document.getElementById("radius").value
+  let volume=4/3*(3.14)*Number(radius)**3
+  let res=document.getElementById("volume")
+  res.value=volume.toFixed(4)
   
 } 
 
