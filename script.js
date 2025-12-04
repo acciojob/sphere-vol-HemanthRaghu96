bf3b77dc788a20844f741c6a7941f82e98d0afbe
Hemanth R <91587889+HemanthRaghu96@users.noreply.github.com>
2025-12-04 19:04:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function volume_sphere() {
-    //Write your code here
-	let radius=document.getElementById("radius").value
-  let volume=4/3*(3.14)*Number(radius)**3
-  let res=document.getElementById("volume")
-  res.value=volume.toFixed(4)
-  
-} 
+function volume_sphere(event) {
+    event.preventDefault(); // stop form refresh
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    let radius = document.getElementById("radius").value;
+    let volume = (4/3) * Math.PI * Math.pow(Number(radius), 3);
+
+    let res = document.getElementById("volume");
+    res.value = volume.toFixed(4);
+}
+
+window.onload = function () {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+};
